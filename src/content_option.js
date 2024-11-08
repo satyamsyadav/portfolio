04f393f3df0e022054ecbe5dbc808633124fbf71
Satyam Yadav <satyamyadav@Satyams-MacBook-Air.local>
2024-11-08 19:42:37 +0530

    satyam

---
+++ src/content_option.js
@@ -0,0 +1,165 @@
+const logotext = "SATYAM YADAV";
+
+import image from "./assets/images/image.jpeg";
+const meta = {
+    title: "Satyam Yadav",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Satyam Yadav",
+    animated: {
+        first: "I am Developer",
+        second: "I Love Frontend",
+        third: "I like Coding",
+    },
+    description: "I am a passionate React developer with hands-on experience in building dynamic, user-friendly web applications. With a solid understanding of HTML, CSS, and JavaScript, I specialize in creating responsive and intuitive interfaces that enhance user experience. Having completed a ReactJS internship at iGAP Technologies, I am proficient in using modern front-end development tools and libraries.",
+    your_img_url: image,
+};
+
+const dataabout = {
+    title: "Something About me",
+    aboutme: "I specialize in creating responsive and intuitive interfaces that enhance user experience. Having completed a ReactJS internship at iGAP Technologies, I am proficient in using modern front-end development tools and libraries. I am skilled in integrating APIs, managing state with Redux, and optimizing performance in web applications. I am constantly learning and staying updated with the latest advancements in the React ecosystem to deliver high-quality solutions.",
+};
+const worktimeline = [{
+        jobtitle: "Frontend Developer intern",
+        where: "Gadhinglaj",
+        date: "2024",
+    },
+    {
+        jobtitle: "Postmen student Expert",
+        where: "Virtual",
+        date: "2024",
+    }
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "C++",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 95,
+    },
+    {
+        name: "HTML",
+        value: 95,
+    },
+    {
+        name: "CSS",
+        value: 98,
+    },
+    {
+        name: "MongoDB",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "UI UX Designer",
+    },
+    {
+        title: "Farmers magic",
+        description: "Specializes in designing and developing the visual and interactive aspects of a web application using technologies like HTML, CSS, JavaScript, and frameworks like React.",
+    },
+    {
+        title: "Car Rental System",
+        description: "Focuses on server-side logic, database management, and ensuring smooth communication between the server, database, and client applications using technologies like Node.js, Express, and databases.",
+    },
+    {
+        title: "Project Management using 'Jira'",
+        description: "Manages project tracking, workflow automation, and issue tracking using JIRA to streamline task management and collaboration within development teams.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://github.com/GurdeepSingh-767/Vehicle-Renting-System/raw/main/public/css/img/img1.png",
+        description: "Car rental System using ReactJS.",
+        link: "https://github.com/293847/Car-Rental-Website",
+    },
+    {
+        img: "https://github.com/293847/Farmers_Magic/blob/main/images/logo11.png?raw=true",
+        description: "Farmers Magic Website",
+        link: "https://github.com/293847/Farmers_Magic",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://github.com/293847/Farmers_Magic/blob/main/images/logo11.png?raw=true",
+        description: "Farmers Magic Website",
+        link: "https://github.com/293847/Farmers_Magic",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "satyamsyadav03@gmail.com",
+    YOUR_FONE: "7028874340",
+    description: "Frontend & backend developer with freelancing.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/satyamsyadav/",
+    facebook: "/",
+    linkedin: "https://linkedin.com/in/satyamsyadav/",
+    twitter: "https://x.com/Satyamyadav3455/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
